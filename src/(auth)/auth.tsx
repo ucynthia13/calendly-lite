10aea51b4a0ad1eaf82f8324e070ad1e015b6ca0
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-01-17 11:44:45 +0200

    add authentication pages and middleware with Clerk integration

---
+++ src/(auth)/auth.tsx
@@ -0,0 +1,5 @@
+export default function AuthLayout({ children }: {children: React.ReactNode}){
+    <div className="min-h-screen flex flex-col justify-center items-center">
+        {children}
+    </div>
+}
