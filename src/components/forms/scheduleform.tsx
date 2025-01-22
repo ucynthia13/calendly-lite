3227c3d9d99c018a8207e0a651bc6622afc77eca
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-01-22 09:47:25 +0200

    enhance ScheduleForm with availability management; implement field array for availabilities and improve timezone selection

--- src/components/forms/scheduleform.tsx
+++ src/components/forms/scheduleform.tsx
@@ -2,15 +2,17 @@
 
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { useFieldArray, useForm } from "react-hook-form"
 import { scheduleFormSchema } from "@/schema/schema"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "../ui/form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
 import { Button } from "../ui/button"
 import Link from "next/link"
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants"
 import { formatTime, formatTimezone } from "@/lib/utils"
-import { Select , SelectContent, SelectItem, SelectTrigger, SelectValue} from "../ui/select"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select"
+import { Plus } from "lucide-react"
+import { Input } from "../ui/input"
 
 type Availability = {
     startTime: string,
@@ -23,7 +25,7 @@ export default function ScheduleForm({ schedule }: { schedule?: { timezone: stri
 
     useEffect(() => {
         const allTimezones = Intl.supportedValuesOf("timeZone")
-        setTimezones(allTimezones.slice(0,5))
+        setTimezones(allTimezones.slice(0, 5))
     }, []);
 
     const form = useForm<z.infer<typeof scheduleFormSchema>>({
@@ -34,6 +36,15 @@ export default function ScheduleForm({ schedule }: { schedule?: { timezone: stri
         }
     })
 
+    const { append: appendAvailability, remove: removeAvailability, fields: availabilityFields } = useFieldArray({
+        name: "availabilities",
+        control: form.control
+    })
+
+    const groupedAvailabilities = Object.groupBy(availabilityFields.map((availabilityField, index) => ({
+        ...availabilityField, index
+    })), availability => availability.dayOfTheWeek
+    )
     const onSubmit = async (values: z.infer<typeof scheduleFormSchema>) => {
         console.log(values.timezone)
     }
@@ -59,7 +70,8 @@ export default function ScheduleForm({ schedule }: { schedule?: { timezone: stri
                                 <SelectContent>
                                     {timezones.map(timezone => (
                                         <SelectItem key={timezone} value={timezone}>
-                                            {timezone} {formatTimezone(timezone)}
+                                            {timezone}
+                                            `${formatTimezone(timezone)}`
                                         </SelectItem>
                                     ))}
                                 </SelectContent>
@@ -68,6 +80,42 @@ export default function ScheduleForm({ schedule }: { schedule?: { timezone: stri
                         </FormItem>
                     )}
                 />
+                {/* Availabitilies */}
+                <div className="grid grid-cols-[auto, 1fr] gap-y-6">
+                    {DAYS_OF_WEEK_IN_ORDER.map(dayOfWeek => (
+                        <Fragment key={dayOfWeek}>
+                            <div className="capitalize text-sm font-semibold">{dayOfWeek.substring(0, 3)}</div>
+                            <div className="flex flex-col gap-2">
+                                <Button type="button" className="size-6 p-1" variant="outline" onClick={() => { }}>
+                                    <Plus className="size-full" />
+                                </Button>
+                                {groupedAvailabilities[dayOfWeek]?.map((field, labelIndex) => (
+                                    <FormField
+                                        control={form.control}
+                                        name={`availabilities.${field.index}.startTime`}
+                                        render={({ field }) => (
+                                            <div className="flex flex-col gap-2">
+                                                <div className="flex gap-2 items-center">
+                                                    <FormItem>
+                                                        <FormLabel>Timezone</FormLabel>
+                                                        <FormControl>
+                                                            <Input className="w-24" aria-label={`${dayOfWeek} Start Time ${labelIndex} ${labelIndex + 1}`} {...field} />
+                                                        </FormControl>
+                                                        <FormDescription>Timezone users will see when booking</FormDescription>
+                                                    </FormItem>
+                                                </div>
+                                                <FormMessage>
+                                                    {form.formState.errors.availabilities?.at?.(field.id)?.root?.message}
+                                                </FormMessage>
+                                            </div>
+                                        )}
+                                    />
+                                ))}
+                            </div>
+                        </Fragment>
+                    ))}
+                </div>
+
                 <div className="flex justify-end gap-2">
                     <Button asChild type="button" variant="outline">
                         <Link href="/events">Cancel</Link>
