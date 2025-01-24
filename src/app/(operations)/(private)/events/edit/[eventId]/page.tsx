7d4d89bf83248158a28825e3a37cdf9d2a9a244c
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-01-24 15:04:49 +0200

    refactor EditEventPage component; streamline parameter destructuring and improve null checks for event retrieval

--- src/app/(operations)/(private)/events/edit/[eventId]/page.tsx
+++ src/app/(operations)/(private)/events/edit/[eventId]/page.tsx
@@ -1,32 +1,36 @@
-import EventForm from "@/components/forms/eventform";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { db } from "@/drizzle/db";
-import { auth } from "@clerk/nextjs/server";
-import { notFound } from "next/navigation";
+import EventForm from "@/components/forms/eventform"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { db } from "@/drizzle/db"
+import { auth } from "@clerk/nextjs/server"
+import { notFound } from "next/navigation"
 
-interface PageProps{
-    params:{
-        eventId: string
-    }
-}
+export const revalidate = 0
 
-export default async function EventEditPage({ params }: PageProps){
-    const { userId, redirectToSignIn } = await auth()
-    if(userId == null) return redirectToSignIn()
-    const event = await db.query.EventTable.findFirst({
-        where: ({ id, clerkUserId}, {and, eq}) => and(eq(clerkUserId, userId), eq(id, params.eventId))
-    })
+export default async function EditEventPage({
+  params: { eventId },
+}: {
+  params: { eventId: string }
+}) {
+  const { userId, redirectToSignIn } = await auth()
+  if (userId == null) return redirectToSignIn()
 
-    if(!event) return notFound()
+  const event = await db.query.EventTable.findFirst({
+    where: ({ id, clerkUserId }, { and, eq }) =>
+      and(eq(clerkUserId, userId), eq(id, eventId)),
+  })
 
-    return(
-        <Card className="max-w-md mx-auto">
-            <CardHeader>
-                <CardTitle>Edit Event</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <EventForm event={{...event, description: event?.description || undefined }}/>
-            </CardContent>
-        </Card>
-    )
+  if (event == null) return notFound()
+
+  return (
+    <Card className="max-w-md mx-auto">
+      <CardHeader>
+        <CardTitle>Edit Event</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <EventForm
+          event={{ ...event, description: event.description || undefined }}
+        />
+      </CardContent>
+    </Card>
+  )
 }
