7100a53765f6c23c06d0fefc95455682c0847005
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-03-24 12:03:37 +0200

    refactor GoogleSVG and Hero components; update SVG size and adjust button padding for improved layout

--- src/components/page/page-hero.tsx
+++ src/components/page/page-hero.tsx
@@ -5,7 +5,7 @@ import GithubSVG from "../../../public/githubicon";
 
 export default function Hero() {
   return (
-    <div className="w-full flex flex-row items-center justify-between px-8 bg-card">
+    <div className="w-full flex flex-row items-center justify-between px-8 bg-card mt-12">
       <div className="flex flex-col max-w-md space-y-6">
         <h1 className="text-7xl opacity-80 font-bold leading-tight text-start">
           Easy Scheduling Ahead
@@ -14,15 +14,15 @@ export default function Hero() {
           Join 20 million professionals who easily book meetings with the #1
           scheduling tool.
         </p>
-        <div className="relative min-h-screen flex flex-col md:flex-row gap-4 md:gap-2 justify-center">
+        <div className="flex flex-col md:flex-row gap-4 md:gap-2 justify-center">
           <Button className="flex items-center text-card rounded-xl h-14">
-            <div className="border p-4 rounded-lg">
+            <div className="border p-2 rounded-lg">
               <GoogleSVG />
             </div>
             <span className="font-semibold">Sign In With Google</span>
           </Button>
           <Button className="flex items-center px-6 py-3 bg-gray-800 text-card hover:bg-gray-900 rounded-xl h-14">
-            <div className="border p-4 rounded-lg">
+            <div className="border p-2 rounded-lg">
               <GithubSVG />
             </div>
             <span className="font-semibold">Sign In With Github</span>
