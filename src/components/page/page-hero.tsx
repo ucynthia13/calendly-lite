1922e3f992d487bb679dff6c021b3f9db83c719f
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-03-24 11:58:18 +0200

    refactor; improve layout and styling for hero

--- src/components/page/page-hero.tsx
+++ src/components/page/page-hero.tsx
@@ -6,34 +6,37 @@ import GithubSVG from "../../../public/githubicon";
 export default function Hero() {
   return (
     <div className="w-full flex flex-row items-center justify-between px-8 bg-card">
-      {/* Left Content */}
       <div className="flex flex-col max-w-md space-y-6">
         <h1 className="text-7xl opacity-80 font-bold leading-tight text-start">
           Easy Scheduling Ahead
         </h1>
         <p className="text-lg opacity-70 text-start">
-          Join 20 million professionals who easily book meetings with the #1 scheduling tool.
+          Join 20 million professionals who easily book meetings with the #1
+          scheduling tool.
         </p>
-        <div className="flex flex-col gap-4">
-          <Button className="flex items-center text-card rounded-lg h-14">
-            <GoogleSVG />
+        <div className="relative min-h-screen flex flex-col md:flex-row gap-4 md:gap-2 justify-center">
+          <Button className="flex items-center text-card rounded-xl h-14">
+            <div className="border p-4 rounded-lg">
+              <GoogleSVG />
+            </div>
             <span className="font-semibold">Sign In With Google</span>
           </Button>
-          <Button className="flex items-center px-6 py-3 bg-gray-800 text-card hover:bg-gray-900 rounded-lg h-14">
-            <GithubSVG />
+          <Button className="flex items-center px-6 py-3 bg-gray-800 text-card hover:bg-gray-900 rounded-xl h-14">
+            <div className="border p-4 rounded-lg">
+              <GithubSVG />
+            </div>
             <span className="font-semibold">Sign In With Github</span>
           </Button>
         </div>
       </div>
 
-      {/* Right Image */}
-      <div className="justify-center items-center hidden md:block">
+      <div className="md:w-1/2 justify-center items-center hidden md:block">
         <Image
           src="/image/h.png"
           alt="Hero Image"
-          width={800}
-          height={800}
-          className="object-cove rounded-lg "
+          width={500}
+          height={500}
+          className="w-full h-full object-cove rounded-lg "
         />
       </div>
     </div>
